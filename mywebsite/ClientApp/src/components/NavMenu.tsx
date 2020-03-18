8f6f3d67751052d349f8afb0eb2be374908ed95c
gmobrice <gmobrice@outlook.com>
2020-03-17 21:13:03 -0300

    First changes on layout; removed sample routing

--- mywebsite/ClientApp/src/components/NavMenu.tsx
+++ mywebsite/ClientApp/src/components/NavMenu.tsx
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
-import { Collapse, Container, Navbar, NavbarBrand, NavbarToggler, NavItem, NavLink } from 'reactstrap';
+import { Collapse, Container, Navbar, NavbarBrand, NavbarToggler, NavItem, NavLink, Media, Nav } from 'reactstrap';
 import { Link } from 'react-router-dom';
-import './NavMenu.css';
+import './NavMenu.scss';
 
 export class NavMenu extends Component<any, any> {
   static displayName = NavMenu.name;
@@ -24,20 +24,34 @@ export class NavMenu extends Component<any, any> {
   render () {
     return (
       <header>
-        <Navbar className="navbar-expand-sm navbar-toggleable-sm ng-white border-bottom box-shadow mb-3" light>
-          <Container>
-            <NavbarBrand tag={Link} to="/">mywebsite</NavbarBrand>
+        <Navbar className="navbar-expand-md navbar-toggleable-md mb-3"> 
+          <Container fluid>
+            <NavbarBrand tag={Link} to="/">
+              <Media object src="https://via.placeholder.com/64" alt="Logo" className="rounded-circle" />
+            </NavbarBrand>
             <NavbarToggler onClick={this.toggleNavbar} className="mr-2" />
-            <Collapse className="d-sm-inline-flex flex-sm-row-reverse" isOpen={!this.state.collapsed} navbar>
+            <Collapse className="d-md-inline-flex flex-md-row-reverse" isOpen={!this.state.collapsed} navbar>
               <ul className="navbar-nav flex-grow">
                 <NavItem>
-                  <NavLink tag={Link} className="text-dark" to="/">Home</NavLink>
+                  <NavLink className="title">About</NavLink>
                 </NavItem>
                 <NavItem>
-                  <NavLink tag={Link} className="text-dark" to="/counter">Counter</NavLink>
+                  <NavLink tag={Link} className="text-dark" to="/">Work</NavLink>
                 </NavItem>
                 <NavItem>
-                  <NavLink tag={Link} className="text-dark" to="/fetch-data">Fetch data</NavLink>
+                  <NavLink tag={Link} className="text-dark" to="/">Personal</NavLink>
+                </NavItem>
+                <NavItem>
+                  <NavLink className="title">Connect with me</NavLink>
+                </NavItem>
+                <NavItem>
+                  <NavLink tag={Link} className="text-dark" to="/">LinkedIn</NavLink>
+                </NavItem>
+                <NavItem>
+                  <NavLink tag={Link} className="text-dark" to="/">GitHub</NavLink>
+                </NavItem>
+                <NavItem>
+                  <NavLink tag={Link} className="text-dark" to="/">E-mail</NavLink>
                 </NavItem>
               </ul>
             </Collapse>
